067f982afd90d279f829a5218e8ad21b12300168
DevDHera <devdhera@gmail.com>
2018-04-11 20:25:58 +0530

    Handles Auth

---
+++ configs/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mongoURI: 'mongodb://devd:devd@ds237989.mlab.com:37989/storybooks-dev',
+    googleClientID: '871437045906-gjq98t8lsmj00pso0tbisfknpvbl5tkr.apps.googleusercontent.com',
+    googleClientSecret: 'YWZQHAqAXM1JOeDcZlybnVE-'
+}
+
+
+//871437045906-gjq98t8lsmj00pso0tbisfknpvbl5tkr.apps.googleusercontent.com
+//YWZQHAqAXM1JOeDcZlybnVE-
+
